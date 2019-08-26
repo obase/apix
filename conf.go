5fb8e8fb0916a0f637821f5729071f9a64e5db9c
hezhaowu <hezhaowu@kingsoft.com>
2019-08-26 10:42:29 +0800

    save

--- conf.go
+++ conf.go
@@ -2,30 +2,34 @@ package apix
 
 import (
 	"github.com/obase/conf"
+	"github.com/obase/ginx/httpcache"
 	"time"
 )
 
 /*服务配置,注意兼容性.Grpc服务添加前缀"grpc."*/
 type Config struct {
-	Name                string        `json:"name" bson:"name" yaml:"name"`                            // 服务名称
-	HttpHost            string        `json:"httpHost" bson:"httpHost" yaml:"httpHost"`                // 默认本机扫描到的第一个私用IP
-	HttpPort            int           `json:"httpPort" bson:"httpPort" yaml:"httpPort"`                // 若为空表示不启用http server
-	HttpKeepAlive       time.Duration `json:"httpKeepAlive" bson:"httpKeepAlive" yaml:"httpKeepAlive"` // 默认不启用
-	HttpCheckTimeout    string        `json:"httpCheckTimeout" bson:"httpCheckTimeout" yaml:"httpCheckTimeout"`
-	HttpCheckInterval   string        `json:"httpCheckInterval" bson:"httpCheckInterval" yaml:"httpCheckInterval"`
-	WbskReadBufferSize  int           `json:"wbskReadBufferSize" bson:"wbskReadBufferSize" yaml:"wbskReadBufferSize"`    // 默认4092
-	WbskWriteBufferSize int           `json:"wbskWriteBufferSize" bson:"wbskWriteBufferSize" yaml:"wbskWriteBufferSize"` // 默认4092
-	WbskNotCheckOrigin  bool          `json:"wbskNotCheckOrigin" bson:"wbskNotCheckOrigin" yaml:"wbskNotCheckOrigin"`    // 默认false
-	GrpcHost            string        `json:"grpcHost" bson:"grpcHost" yaml:"grpcHost"`                                  // 默认本机扫描到的第一个私用IP
-	GrpcPort            int           `json:"grpcPort" bson:"grpcPort" yaml:"grpcPort"`                                  // 若为空表示不启用grpc server
-	GrpcKeepAlive       time.Duration `json:"grpcKeepAlive" bson:"grpcKeepAlive" yaml:"grpcKeepAlive"`                   // 默认不启用
-	GrpcCheckTimeout    string        `json:"grpcCheckTimeout" bson:"grpcCheckTimeout" yaml:"grpcCheckTimeout"`
-	GrpcCheckInterval   string        `json:"grpcCheckInterval" bson:"grpcCheckInterval" yaml:"grpcCheckInterval"`
+	Name                string            `json:"name" bson:"name" yaml:"name"`                            // 服务名称
+	HttpHost            string            `json:"httpHost" bson:"httpHost" yaml:"httpHost"`                // 默认本机扫描到的第一个私用IP
+	HttpPort            int               `json:"httpPort" bson:"httpPort" yaml:"httpPort"`                // 若为空表示不启用http server
+	HttpKeepAlive       time.Duration     `json:"httpKeepAlive" bson:"httpKeepAlive" yaml:"httpKeepAlive"` // 默认不启用
+	HttpCheckTimeout    string            `json:"httpCheckTimeout" bson:"httpCheckTimeout" yaml:"httpCheckTimeout"`
+	HttpCheckInterval   string            `json:"httpCheckInterval" bson:"httpCheckInterval" yaml:"httpCheckInterval"`
+	WbskReadBufferSize  int               `json:"wbskReadBufferSize" bson:"wbskReadBufferSize" yaml:"wbskReadBufferSize"`    // 默认4092
+	WbskWriteBufferSize int               `json:"wbskWriteBufferSize" bson:"wbskWriteBufferSize" yaml:"wbskWriteBufferSize"` // 默认4092
+	WbskNotCheckOrigin  bool              `json:"wbskNotCheckOrigin" bson:"wbskNotCheckOrigin" yaml:"wbskNotCheckOrigin"`    // 默认false
+	GrpcHost            string            `json:"grpcHost" bson:"grpcHost" yaml:"grpcHost"`                                  // 默认本机扫描到的第一个私用IP
+	GrpcPort            int               `json:"grpcPort" bson:"grpcPort" yaml:"grpcPort"`                                  // 若为空表示不启用grpc server
+	GrpcKeepAlive       time.Duration     `json:"grpcKeepAlive" bson:"grpcKeepAlive" yaml:"grpcKeepAlive"`                   // 默认不启用
+	GrpcCheckTimeout    string            `json:"grpcCheckTimeout" bson:"grpcCheckTimeout" yaml:"grpcCheckTimeout"`
+	GrpcCheckInterval   string            `json:"grpcCheckInterval" bson:"grpcCheckInterval" yaml:"grpcCheckInterval"`
+	HttpCache           *httpcache.Config `json:"httpCache" bson:"httpCache" yaml:"httpCache"`
+	HttpPlugin          *httpcache.Config `json:"httpPlugin" bson:"httpPlugin" yaml:"httpPlugin"`
+	HttpEntry
 }
 
 const CKEY = "service"
 
-func LoadConfig() (*Config) {
+func LoadConfig() *Config {
 	var config *Config
 	if ok := conf.Scan(CKEY, &config); !ok {
 		return nil
--- go.mod
+++ go.mod
@@ -8,7 +8,8 @@ require (
 	github.com/gorilla/websocket v1.4.0
 	github.com/obase/api v1.3.5
 	github.com/obase/center v1.3.5
-	github.com/obase/conf v1.3.5
+	github.com/obase/conf v1.4.2
+	github.com/obase/ginx v0.0.8
 	github.com/obase/log v1.3.5
 	google.golang.org/grpc v1.23.0
 )
