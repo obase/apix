a7899cbf3262d59d428ed10500de5e31b3e021b6
hezhaowu <hezhaowu@kingsoft.com>
2019-08-27 22:51:34 +0800

    save

--- kits.go
+++ kits.go
@@ -7,10 +7,7 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/obase/api"
 	"github.com/obase/log"
-	"net"
 	"net/http"
-	"os"
-	"time"
 )
 
 const (
@@ -142,55 +139,9 @@ func CreateSocketUpgrader(conf *Config) *websocket.Upgrader {
 	return upgrader
 }
 
-var PrivateAddress = func(def string) (ret string) {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return def
-	}
-	for _, addr := range addrs {
-		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ip4 := ipnet.IP.To4(); ip4 != nil {
-				// 必须私有网段
-				if (ip4[0] == 10) || (ip4[0] == 172 && ip4[1] >= 16 && ip4[1] <= 31) || (ip4[0] == 192 && ip4[1] == 168) {
-					return ip4.String()
-				}
-			}
-		}
-	}
-	return def
-}("127.0.0.1")
-
 func Errorf(code int, format string, args ...interface{}) error {
 	return &api.Response{
 		Code: code,
 		Msg:  fmt.Sprintf(format, args...),
 	}
-}
-
-// tcpKeepAliveListener sets TCP keep-alive timeouts on accepted
-// connections. It's used by ListenAndServe and ListenAndServeTLS so
-// dead TCP connections (e.g. closing laptop mid-download) eventually
-// go away.
-type tcpKeepAliveListener struct {
-	*net.TCPListener
-	KeepAlivePeriod time.Duration
-}
-
-func (ln tcpKeepAliveListener) Accept() (net.Conn, error) {
-	tc, err := ln.AcceptTCP()
-	if err != nil {
-		return nil, err
-	}
-	tc.SetKeepAlive(true)
-	if ln.KeepAlivePeriod == 0 {
-		tc.SetKeepAlivePeriod(3 * time.Minute)
-	} else {
-		tc.SetKeepAlivePeriod(ln.KeepAlivePeriod)
-	}
-	return tc, nil
-}
-
-func GetFile(l net.Listener) *os.File {
-	file, _ := l.(*net.TCPListener).File()
-	return file
 }
