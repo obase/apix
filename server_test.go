b6106486f69c9970d794edce1cc4fb3194d7e898
hezhaowu <hezhaowu@kingsoft.com>
2019-08-28 12:36:38 +0800

    save

--- server_test.go
+++ server_test.go
@@ -10,14 +10,15 @@ import (
 
 func TestNewServer(t *testing.T) {
 	server := NewServer()
+	server.Use(func(context *gin.Context) {
+		fmt.Println("this is use 1...")
+	}, func(context *gin.Context) {
+		fmt.Println("this is use 2...")
+	})
 	server.Routes(func(server *ginx.Server) {
 		server.GET("/now", func(context *gin.Context) {
 			fmt.Fprintf(context.Writer, "current time: %v\n", time.Now().Format("2006-01-02 15:04:05.777"))
 		})
 	})
 	server.Serve()
-}
-
-func test(args ...interface{}) {
-	fmt.Printf("%v", args == nil)
 }
